fb62690aceb3b57825fc5e0310781e38f8fc21fd
XuzhM <xuzhaoming2048@163.com>
2018-04-23 15:41:01 +0800

    第一次提价

---
+++ TestNTier/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestNTier {
+	public class FilterConfig {
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
